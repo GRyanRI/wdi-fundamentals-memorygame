4bf2f7ad66c99bddd68b063165e6c5af9a3eef64
GRyanRI <gregorywryan@gmail.com>
2018-10-10 10:03:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,31 @@
 console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+	{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+ ];
 
 var cardsInPlay = [];
 
@@ -14,10 +38,13 @@ var checkForMatch = function(){
 	}
 var flipCard = function (cardId) {
 
-	cardsInPlay.push(cards[cardId])
-	checkForMatch()
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log("User flipped " + cards[cardId])
+	checkForMatch();
+
+	console.log("User flipped " + cards[cardId].rank + " image path: " + cards[cardId].cardImage + " suit: " + cards[cardId].suit);
+
+
 }
 
 
