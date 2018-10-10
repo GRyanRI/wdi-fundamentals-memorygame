bc72f45bd4affc107bf89300a0df3e7c17e8f3ca
GRyanRI <gregorywryan@gmail.com>
2018-10-10 13:10:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,24 +30,40 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
+		
 		if (cardsInPlay[0]===cardsInPlay[1]) {
 			alert("You found a match!")
 		} else {
 			alert("Sorry, try again.")
 		}
 	}
-var flipCard = function (cardId) {
+var flipCard = function () {
+
+
+	var cardId = this.getAttribute('data-id');
+
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId].rank);
 
-	checkForMatch();
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	} 
 
 	console.log("User flipped " + cards[cardId].rank + " image path: " + cards[cardId].cardImage + " suit: " + cards[cardId].suit);
 
 
 }
 
-
-flipCard(0)
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute("src","images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener("click",flipCard);//trigger flipCard function when an item is clicked
+		document.getElementById("game-board").appendChild(cardElement)
+	}
+}
+createBoard()
 
 
